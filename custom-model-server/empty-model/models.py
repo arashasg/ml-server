9bc443fded99c272b4bb19316f977df43d32614c
arashpirelly <arash.a@pirelly.com>
2023-09-12 20:33:49 +0330

    Add custom model inference request

--- custom-model-server/empty-model/models.py
+++ custom-model-server/empty-model/models.py
@@ -6,7 +6,5 @@ class MyCustomRuntime(MLModel):
   async def load(self) -> bool:
     return True
 
-  async def predict(self, payload: InferenceRequest) -> InferenceResponse:
-    return InferenceResponse(
-      outputs=[1, 2, 3]
-    )
+  async def predict(self, payload: InferenceRequest) -> int:
+    return 1 + 1
