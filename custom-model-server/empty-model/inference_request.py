91b0c09e715f9a2ece5b9f3d475b4526a49c36da
arashpirelly <arash.a@pirelly.com>
2023-09-12 21:20:50 +0330

    Add custom model inference request

--- custom-model-server/empty-model/inference_request.py
+++ custom-model-server/empty-model/inference_request.py
@@ -8,7 +8,7 @@ from mlserver.codecs import NumpyCodec
 x_0 = np.array([28.0])
 inference_request = InferenceRequest(
     inputs=[
-        NumpyCodec.encode_input(name="marriage", payload=x_0)
+        NumpyCodec.encode_input(name="", payload=x_0)
     ]
 )
 res = requests.post("http://localhost:8080/v2/models/empty-model/infer", json=inference_request.dict()).json()
--- custom-model-server/empty-model/models.py
+++ custom-model-server/empty-model/models.py
@@ -6,7 +6,7 @@ class MyCustomRuntime(MLModel):
   async def load(self) -> bool:
     return True
 
-  async def predict(self, payload: InferenceRequest) -> int:
+  async def predict(self, payload: InferenceRequest) -> InferenceResponse:
     print("\n \n \n \n")
     print(payload)
     print("\n\n\n\n")
